902fa2321201229cc706de2c3dc61ebf018753c2
ZharkovMihail <32340066+ZharkovMihail@users.noreply.github.com>
2018-12-18 22:35:44 +0300

    Update main.cpp

--- task5/main.cpp
+++ task5/main.cpp
@@ -21,11 +21,31 @@ public:
 		return res;
 	}
 };
+void foo(Atomic<int> &x) {
+	for (int i = 0; i < 10000; ++i) {
+		x =x+ i;
+	}
+}
+
+void bar(Atomic<int> &x) {
+	for (int i = 0; i < 10000; ++i) {
+		x =x- i;
+	}
+}
+
 
 int main() {
 	Atomic <int> a(1);
-	Atomic <int> b(a+2);
+	Atomic <int> b(a + 2);
 	cout << a + b;
+	Atomic <int> x(1);
+	std::thread th1([&x]() {foo(x); });
+	std::thread th2([&x]() {bar(x); });
+
+	th1.join();
+	th2.join();
+
+	cout << x << endl;
 	system("pause");
 	return 0;
 }
